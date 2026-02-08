02bef2793a2c27877125ad695fb878d07f4c37b2
JamCatAI <superbusmalmo@gmail.com>
2026-02-08 19:41:10 +0100

    feat: add JamCat click game and leaderboard section

---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import JamCatPopGame from '$lib/JamCatPopGame.svelte';
+</script>
+
+<svelte:head>
+	<title>JamCat</title>
+	<meta name="description" content="Tap JamCat and climb the country leaderboard" />
+</svelte:head>
+
+<JamCatPopGame />
